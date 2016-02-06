9e35a21192f756f796b7130fb76a6c64b6ccde3c
Alexander Trauzzi <atrauzzi@sigma>
2016-02-06 17:29:01 -0600

    Working on crons.

--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -4,9 +4,10 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Cache;
 
-class Kernel extends ConsoleKernel
-{
+
+class Kernel extends ConsoleKernel {
     /**
      * The Artisan commands provided by your application.
      *
@@ -22,9 +23,16 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
-        $schedule->command('inspire')
-                 ->hourly();
+    protected function schedule(Schedule $schedule) {
+
+        $schedule->call(function () {
+            Cache::put('last-message', 'Cron says hello!', 5);
+        })->everyMinute();
+
+        $schedule
+            ->command('inspire')
+            ->hourly()
+        ;
     }
+
 }
--- php.Dockerfile
+++ php.Dockerfile
@@ -7,6 +7,7 @@ RUN apt-get update -y
 RUN apt-get install -y \
     curl \
 	git \
+    cron \
     php5-pgsql \
     php5-redis \
     php5-json \
@@ -23,6 +24,9 @@ RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local
 ADD ./laravel /var/www
 ADD /resources/php-fpm.conf /etc/php5/fpm/php-fpm.conf
 ADD /resources/php-fpm.www.conf /etc/php5/fpm/pool.d/www.conf
+
+ADD /resources/crontab /etc/cron.d/laravel
+RUN chmod 644 /etc/cron.d/laravel
 
 ADD /resources/artisan /usr/local/bin/artisan
 RUN chmod +x /usr/local/bin/artisan
