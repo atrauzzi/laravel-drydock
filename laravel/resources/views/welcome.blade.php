aef351f0ceaba3505bde66b5fd3d8418d13bb5b9
Alexander Trauzzi <atrauzzi@sigma>
2016-02-06 15:42:49 -0600

    More welcome page improvements.

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -32,15 +32,15 @@
 
             .title {
                 font-size: 96px;
+                font-weight: normal;
             }
 
             a:link, a:visited {
+                text-decoration: none;
                 color: #002585;
                 font-weight: bold;
-            }
-
-            div {
-                font-weight: bold;
+                margin-left: 5px;
+                margin-right: 5px;
             }
 
         </style>
@@ -49,10 +49,11 @@
         <div class="container">
             <div class="content">
 
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel Drydock</div>
 
                 <a href="/dev/info">Environment Information</a>
                 <a href="http://{{ Request::getHost() }}:8081">RabbitMQ</a>
+                <a href="http://github.com/atrauzzi/laravel-drydock">Github</a>
 
                 <hr />
 
