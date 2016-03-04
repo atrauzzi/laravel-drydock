0310257d2deb07431ba4a7365339474ee3ba17d9
Alexander Trauzzi <atrauzzi@gmail.com>
2016-03-03 18:47:15 -0600

    Closes #20.

--- laravel/app/Exceptions/Handler.php
+++ laravel/app/Exceptions/Handler.php
@@ -6,8 +6,7 @@ use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Illuminate\Foundation\Validation\ValidationException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 
